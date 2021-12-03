bdcb7b869e1e2093b66f481416e85cdbfc9d16fe
zefan.dou <zefan.dou@thundersoft.com>
2021-12-03 16:44:07 +0800

    初始化

---
+++ IM_Server/src/test/java/com/example/im_server/ImServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.im_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
